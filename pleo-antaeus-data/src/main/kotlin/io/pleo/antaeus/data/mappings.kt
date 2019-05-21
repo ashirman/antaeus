fd22e6b6f78d24b41baa7a63a26a2e0e5c96b52e
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-05-21 18:56:31 +1000

    get actual customer status from DB

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/mappings.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/mappings.kt
@@ -7,22 +7,24 @@ package io.pleo.antaeus.data
 
 import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.models.CustomerStatus
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
 import org.jetbrains.exposed.sql.ResultRow
 
 fun ResultRow.toInvoice(): Invoice = Invoice(
-    id = this[InvoiceTable.id],
-    amount = Money(
-        value = this[InvoiceTable.value],
-        currency = Currency.valueOf(this[InvoiceTable.currency])
-    ),
-    status = InvoiceStatus.valueOf(this[InvoiceTable.status]),
-    customerId = this[InvoiceTable.customerId]
+        id = this[InvoiceTable.id],
+        amount = Money(
+                value = this[InvoiceTable.value],
+                currency = Currency.valueOf(this[InvoiceTable.currency])
+        ),
+        status = InvoiceStatus.valueOf(this[InvoiceTable.status]),
+        customerId = this[InvoiceTable.customerId]
 )
 
 fun ResultRow.toCustomer(): Customer = Customer(
-    id = this[CustomerTable.id],
-    currency = Currency.valueOf(this[CustomerTable.currency])
+        id = this[CustomerTable.id],
+        currency = Currency.valueOf(this[CustomerTable.currency]),
+        status = CustomerStatus.valueOf(this[CustomerTable.currency])
 )
