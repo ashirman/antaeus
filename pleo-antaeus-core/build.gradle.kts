b868aa9b14cc83dbbfa5f5eed643534cdccc0199
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-05-21 15:57:45 +1000

    add quartz scheduler dependency

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -7,4 +7,5 @@ kotlinProject()
 dependencies {
     implementation(project(":pleo-antaeus-data"))
     compile(project(":pleo-antaeus-models"))
+    implementation("org.quartz-scheduler:quartz:2.3.1")
 }
