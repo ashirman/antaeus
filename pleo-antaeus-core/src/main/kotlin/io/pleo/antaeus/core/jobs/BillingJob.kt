cecff3ae836aed739bee044df276d4be0200dfbd
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-05-21 23:12:45 +1000

    initial version of BillingJob + BillingService using Quartz

---
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/jobs/BillingJob.kt
@@ -0,0 +1,78 @@
+package io.pleo.antaeus.core.jobs
+
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
+import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.core.services.CustomerService
+import io.pleo.antaeus.core.services.InvoiceService
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import org.quartz.Job
+import org.quartz.JobExecutionContext
+import org.slf4j.LoggerFactory
+
+internal class BillingJob : Job {
+    companion object {
+        const val PAYMENT_PROVIDER = "paymentProvider"
+        const val INVOICE_SERVICE = "invoiceService"
+        const val CUSTOMER_SERVICE = "customerService"
+        const val INVOICE_ID = "invoiceId"
+
+        // see https://www.baeldung.com/kotlin-logging
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        @JvmStatic
+        private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+    }
+
+    override fun execute(context: JobExecutionContext) {
+        val paymentProvider = context.scheduler.context[PAYMENT_PROVIDER] as PaymentProvider
+        val invoiceService = context.scheduler.context[PAYMENT_PROVIDER] as InvoiceService
+        val customerService = context.scheduler.context[CUSTOMER_SERVICE] as CustomerService
+
+        val invoiceId = context.jobDetail.jobDataMap.getIntValue(INVOICE_ID)
+
+        var invoice: Invoice? = null
+        try {
+            invoice = invoiceService.fetch(invoiceId)
+
+            // need to check the invoice status right before provider's charge() call.
+            // the job could be planned long time ago and the invoice can be processed/cancelled already somehow
+            // (eg alternate way of payments, via support resolution, etc).
+            if (invoice.status == InvoiceStatus.PENDING) {
+                if (paymentProvider.charge(invoice)) {
+                    invoiceService.update(invoice.copy(status = InvoiceStatus.PAID))
+                } else {
+                    // customer has not enough money to be able to pay the invoice.
+                    //TODO process it somehow
+                }
+            } else {
+                logger.info("invoice ${invoice.id} processing has been skipped due to ${invoice.status} status")
+            }
+        } catch (cnfe: CustomerNotFoundException) {
+            // when no customer has the given id.
+            deleteCurrentJobFromScheduling(context)
+
+            invoice?.let {
+                logger.info("customer ${it.customerId} is not available on")
+                customerService.deactivateCustomer(it.customerId)
+            }
+        } catch (cme: CurrencyMismatchException) {
+            // when the currency does not match the customer account.
+            //convert currency and try to pay it as another invoice.
+            //TODO add convertion service and align invoice currency with customer currency
+        } catch (ne: NetworkException) {
+            // when a network error happens.
+            // retry payment after some time, 3rd part payment provider may not be available.
+            //TODO add numOfRepeats support. retry payments after a while
+        } catch (infe: InvoiceNotFoundException) {
+            logger.info("invoice ${invoice?.id} can not be found. associated job ${context.jobDetail.key} will be deleted")
+            deleteCurrentJobFromScheduling(context)
+        }
+    }
+
+    private fun deleteCurrentJobFromScheduling(context: JobExecutionContext) {
+        context.scheduler.deleteJob(context.jobDetail.key)
+    }
+}
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,44 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.core.jobs.BillingJob
+import io.pleo.antaeus.models.Invoice
+import org.quartz.CronScheduleBuilder
+import org.quartz.JobBuilder
+import org.quartz.Scheduler
+import org.quartz.TriggerBuilder
+import org.quartz.impl.StdSchedulerFactory
 
-class BillingService(
-    private val paymentProvider: PaymentProvider
-) {
-   // TODO - Add code e.g. here
+class BillingService(paymentProvider: PaymentProvider,
+                     invoiceService: InvoiceService,
+                     customerService: CustomerService,
+                     private val scheduler: Scheduler = StdSchedulerFactory().scheduler) {
+
+    init {
+        //using scheduler context is the simplest way to pass instance variables (service instances) to further job instances without using DI frameworks
+        //and/or any kind of global static objects. @see https://stackoverflow.com/questions/12777057/how-to-pass-instance-variables-into-quartz-job
+        //keys/values need to be consistent with what is retrieved in BillingJob class
+        scheduler.context[BillingJob.PAYMENT_PROVIDER] = paymentProvider
+        scheduler.context[BillingJob.INVOICE_SERVICE] = invoiceService
+        scheduler.context[BillingJob.CUSTOMER_SERVICE] = customerService
+
+        if (!scheduler.isStarted) scheduler.start()
+    }
+
+    fun scheduleInvoicePayment(invoice: Invoice, cronSchedule: String = "0 0 12 1 1/1 ? *") {
+        scheduler.scheduleJob(buildJobDetail(invoice), buildTrigger(invoice, cronSchedule))
+    }
+
+    private fun buildTrigger(invoice: Invoice, cronSchedule: String) = TriggerBuilder
+            .newTrigger()
+            .withIdentity("trigger_invoice_" + invoice.id)
+            .withSchedule(CronScheduleBuilder.cronSchedule(cronSchedule))
+            .build()
+
+    private fun buildJobDetail(invoice: Invoice) = JobBuilder
+            .newJob()
+            .ofType(BillingJob::class.java)
+            .withIdentity("job_invoice_" + invoice.id)
+            .usingJobData(BillingJob.INVOICE_ID, invoice.id)
+            .build()
 }
