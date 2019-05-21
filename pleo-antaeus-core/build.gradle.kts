2663cad2f738ddb6c6634907d51814fb90226266
Alexey Shirmanov <ashirmanov@atlassian.com>
2019-05-21 16:17:39 +1000

    add jackson object mapper dependency

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -8,4 +8,5 @@ dependencies {
     implementation(project(":pleo-antaeus-data"))
     compile(project(":pleo-antaeus-models"))
     implementation("org.quartz-scheduler:quartz:2.3.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
 }
